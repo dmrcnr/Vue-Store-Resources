1957f0772f169b94de942dab55745a0df1ed2b1b
Caner Demir <dmrcnr@gmail.com>
2023-07-05 11:37:03 +0300

    learning resource component created

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <LearningResources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    LearningResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Search engine',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
